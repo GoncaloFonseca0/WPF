ae39207767184812da4f8acf5209452a8f10ab36
GoncaloFonseca0 <goncalofoneseca123@gmail.com>
2024-06-03 16:09:31 +0100

    Update Jogo dos dados

--- 29Abril/Controller/Controller.cs
+++ 29Abril/Controller/Controller.cs
@@ -11,18 +11,54 @@ namespace _29Abril.Controller
 {
     public class Controller : UIElement
     {
+        public Cmd cmdRolar { get; set; }  
+        public ClsJogoDados myJogo { get; set; }
         public Cmd cmdNavega { get; set; }
         public Cmd cmdSair { get; set; }
 
         public Cmd cmdSelos { get; set; }
         public Controller()
         {
+
+            myJogo=new ClsJogoDados();
             cmdSair = new Cmd(canSair, Sair);
             cmdNavega = new Cmd(canNavega, Navega);
             cmdSelos = new Cmd(canTrocaSelos, TrocaSelos);
+            cmdRolar = new Cmd(canRolar, Rolar);
         }
 
         MainWindow main = (MainWindow)App.Current.MainWindow;
+
+        public bool canRolar(object parameter)
+        {
+
+
+
+            if (parameter == null) return false;
+            if ( int.TryParse(parameter.ToString(), out int dado))
+            {
+                if (dado > 0) return true;
+
+
+            }
+           
+            return false;
+        }
+
+        public void Rolar(Object parameter)
+        {
+
+
+            if (parameter == null) return;
+            if (int.TryParse(parameter.ToString(), out int aposta))
+            {
+                myJogo.rolar(aposta);
+                Dados pag = (Dados)main.frame.Content;
+                if (pag.txtmontante.Text == "0") pag.slider.Value = 0;
+
+            }
+           
+        }
         public bool canNavega(Object parameter)
         {
             string destino = parameter.ToString();
@@ -38,7 +74,7 @@ namespace _29Abril.Controller
                     main.frame.Source = new Uri(@"/View/Inicio.xaml", UriKind.Relative);
                     break;
                 case "JogoDados":
-                    main.frame.Source = new Uri(@"/View/JogoDados.xaml", UriKind.Relative);
+                    main.frame.Source = new Uri(@"/View/Dados.xaml", UriKind.Relative);
                     break;
                 case "Selos":
                     main.frame.Source = new Uri(@"/View/Selos.xaml", UriKind.Relative);
